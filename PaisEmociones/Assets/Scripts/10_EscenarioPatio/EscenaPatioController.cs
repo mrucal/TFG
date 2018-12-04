8855c76eb0e8f4e9dbf6c9bd84fdbb1a719db099
mrucal <rucalgr@gmail.com>
2018-12-04 13:45:17 +0100

    Añadida escena patio

---
+++ PaisEmociones/Assets/Scripts/10_EscenarioPatio/EscenaPatioController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenaPatioController  : MonoBehaviour {
+
+    public GameObject personaje;
+
+    private const float t_sig_escena = 2f;
+
+    private const float td = 0f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Inicio()
+    {
+        StartCoroutine(SiguienteEscena("07_EscenaCastillo", t_sig_escena));
+    }
+
+    public IEnumerator SiguienteEscena(string escena, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(escena);
+    }
+}
