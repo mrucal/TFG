8db98c263cc497718f5b0fc1a0b05dd7c6e8b6ae
mrucal <rucalgr@gmail.com>
2019-01-27 03:47:15 +0100

    prototipo laberinto

---
+++ PaisEmociones/Assets/Scripts/06_EscenarioLaberinto/PasilloTexturas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PasilloTexturas  : MonoBehaviour {
+    
+    public Texture2D[] texturas_pasillo_dif0;
+    public Texture2D[] texturas_pasillo_dif1;
+    public Texture2D[] texturas_pasillo_dif2;
+
+    public Texture2D[][] texturas_pasillo;
+
+    void Start () {
+        texturas_pasillo = new Texture2D[3][];
+        texturas_pasillo[0] = texturas_pasillo_dif0;
+        texturas_pasillo[1] = texturas_pasillo_dif1;
+        texturas_pasillo[2] = texturas_pasillo_dif2;
+    }
+
+    private void OnMouseDown()
+    {
+    }
+
+    public Texture2D obtenerTextura(int dificultad, int i)
+    {
+        //print("break");
+        return texturas_pasillo[dificultad][i];
+    }
+
+
+
+}
